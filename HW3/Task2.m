72179092745c32b5c8f590f9e959205ce72ca721
Akash Trehan <akash.trehan123@gmail.com>
2016-10-16 03:01:29 +0530

    Added code and graphs

--- HW3/Task2.m
+++ HW3/Task2.m
@@ -1,15 +1,19 @@
+clear;
 N = [5 10 20 40 60 80 100 500 1000 10000];
+%defining the matrices for storing the errors
 ml = zeros(100,10);
 bay = zeros(100,10);
 for k=1:10
 for c = 1:100
 X = rand(N(k),1);
+%getting the data set
 Y = -0.2*log(X);
+%Calculating the errors and storing in the corresponding matrix
 ml(c,k) = abs((N(k)/sum(Y)- 5)/5);
 bay(c,k) = abs(((N(k) + 5.5)/(sum(Y)+1) - 5)/5);
 end
 end
-
+%Plotting the graphs
 subplot(2,1,1) 
 boxplot(ml);
 title('Maximum Likelihood');
@@ -22,12 +26,3 @@ title('Posterior Mean');
 set(gca,'XTickLabel',N);
 xlabel('N');
 ylabel('Relative Error');
-
-%  for c=1:10
-%      %mk = zeros(100,2);
-%      %mk(:,1) = ml(:,c);
-%      %mk(:,2) = bay(:,c);
-%      mk = zeros(10,1);
-%      mk(c,1) = (median(ml(:,c)) < median(bay(:,c)));     %boxplot(mk);
-% end
-% mk
--- HW3/ques3/Task3.m
+++ HW3/ques3/Task3.m
@@ -1,12 +1,17 @@
+clear;
 N = 1000000;
 R = 10;
+% Picking theta from uniform distribution
 U = rand(1,N)*2*pi;
 X = zeros(2,N);
+% Initialising 2-D variable
 X(1,:) = R*cos(U);
 X(2,:) = R*sin(U);
 
 ml = zeros(2,1);
+% Calculating Mu
 ml(1,1) = sum(X(1,:))/N;
 ml(2,1) = sum(X(2,:))/N;
-ml;
-cov(X(1,:)',X(2,:)');
+ml
+% Calculating Covariance Matrix
+cov(X(1,:)',X(2,:)')
