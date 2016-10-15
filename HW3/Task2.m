dda973bbfe501d299fc541af2eb84f30efc4cfcf
Akash Trehan <akash.trehan123@gmail.com>
2016-10-15 19:22:35 +0530

    Added Task2

---
+++ HW3/Task2.m
@@ -0,0 +1,19 @@
+N = [5 10 20 40 60 80 100 500 1000 10000];
+ml = zeros(100,10);
+bay = zeros(100,10);
+for k=1:10
+for c = 1:100
+X = rand(N(k),1);
+Y = -0.2*log(X);
+ml(c,k) = (N(k)/sum(Y)- 5)/5;
+bay(c,k) = ((sum(Y) + 5.5)/(N(k)+1) - 5)/5;
+end
+end
+subplot(2,1,1) 
+boxplot(ml);
+title('Maximum Likelihood');
+set(gca,'XTickLabel',N);
+subplot(2,1,2) 
+boxplot(bay);
+title('Bayesian');
+set(gca,'XTickLabel',N);
