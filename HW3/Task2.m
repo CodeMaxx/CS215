6b8b1878614aa73d833c4dd819cad51a09992136
Akash Trehan <akash.trehan123@gmail.com>
2016-10-16 01:32:09 +0530

    Fixed task2

--- HW3/Task2.m
+++ HW3/Task2.m
@@ -5,10 +5,11 @@ for k=1:10
 for c = 1:100
 X = rand(N(k),1);
 Y = -0.2*log(X);
-ml(c,k) = (N(k)/sum(Y)- 5)/5;
-bay(c,k) = ((N(k)+1)/(sum(Y) + 5.5) - 5)/5;
+ml(c,k) = abs((N(k)/sum(Y)- 5)/5);
+bay(c,k) = abs(((N(k) + 5.5)/(sum(Y)+1) - 5)/5);
 end
 end
+
 subplot(2,1,1) 
 boxplot(ml);
 title('Maximum Likelihood');
@@ -17,7 +18,16 @@ xlabel('N');
 ylabel('Relative Error');
 subplot(2,1,2) 
 boxplot(bay);
-title('Bayesian');
+title('Posterior Mean');
 set(gca,'XTickLabel',N);
 xlabel('N');
 ylabel('Relative Error');
+
+%  for c=1:10
+%      %mk = zeros(100,2);
+%      %mk(:,1) = ml(:,c);
+%      %mk(:,2) = bay(:,c);
+%      mk = zeros(10,1);
+%      mk(c,1) = (median(ml(:,c)) < median(bay(:,c)));     %boxplot(mk);
+% end
+% mk
