f1978d25c5ad74505562916af8e1bebf8dc39b2b
Akash Trehan <akash.trehan123@gmail.com>
2016-11-07 05:14:56 +0530

    Update q1 code

--- HW4/q1.m
+++ HW4/q1.m
@@ -3,14 +3,10 @@ pis = zeros(1,8);
 
 for j=1:8
 n = 10^j;
-
 X = 2*single(rand(2,n)) - 1;
-
 in = 0;
 for i = X
-if(norm(i) <= 1)
-    in = in + 1;
-end
+in = in + (norm(i) <= 1);
 end
 pis(1,j) = 4*in/n;
 end
